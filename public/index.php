1de11cc7753c3f4a387968b8e967e174aca17459
David Desberg <david@daviddesberg.com>
2016-09-19 00:17:44 -0500

    letter

--- public/index.php
+++ public/index.php
@@ -14,14 +14,28 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Letter from the Secretary General</h1>
-                <p>
-                <h3>Dear Ya'll!</h3>
-                Letter coming soon
-                 <br><br>
-                Sincerely,<br>
-                Secgen
-                </p>
+                <h1><span class="illinois-blue-text">Letter from the</span> <span class="illinois-orange-text">Secretary-General</span></h1>
+                <h4>Sunday, September 18, 2016</h4><br>
+                <div>
+                    <p>
+                    Prospective Advisors and Delegates,</p>
+
+                    <p style="text-indent: 30px;">
+                    Welcome to the twenty-second session of Model United Nations at the University of Illinois (MUNI XXII)! The conference will be held between March 10-12, 2016 at the University of Illinois at Urbana-Champaign.
+                    </p>
+
+                    <p style="text-indent: 30px;">
+                    We have been working diligently to ensure that you have the best experience on the Midwestern circuit. We are currently working on redesigning our website and streamlining our registration process. Our staff would like to thank you for being patient while we update our website. As committees are finalized and when registration opens on October 1, 2016, the website will be updated and new features will be introduced. Be sure to check regularly for updates.
+                    </p>
+
+                    <p style="text-indent: 30px;">
+                    On behalf of the Secretariat, I hope you consider adding MUNI XXII to your schedule. We take great pride in our conference and we hope to see you in March! If you have any questions about registering for MUNI XXII, please do not hesitate to contact our Undersecretary-General of Registration at registration@illinoismun.org.
+                    </p>
+
+                    Sincerely,<br>
+                    Stuart J. Polen<br>
+                    Secretary General MUNI XXII
+                </div>
             </div>
         </div>
     </div>
