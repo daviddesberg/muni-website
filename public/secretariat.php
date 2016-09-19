0e8c7af5b22a035f408f694fdad7170e46cad094
David Desberg <david@daviddesberg.com>
2016-09-19 13:39:42 -0500

    temp sec list

--- public/secretariat.php
+++ public/secretariat.php
@@ -14,7 +14,17 @@
             <div class="col-lg-12">
                 <h1>Secretariat</h1>
                 <p>
-                    Secretariat list coming soon.
+                    <span class="illinois-blue-text">Stuart Polen</span> – <span class="illinois-orange-text">Secretary-General</span></span><br>
+                    <span class="illinois-blue-text">Spencer Haydary – </span><span class="illinois-orange-text">Chief of Staff</span><br>
+                    <span class="illinois-blue-text">John Hall – </span><span class="illinois-orange-text">Undersecretary-General of Registration</span><br>
+                    <span class="illinois-blue-text">Danny Benson – </span><span class="illinois-orange-text">Undersecretary-General of Logistics and Operations</span><br>
+                    <span class="illinois-blue-text">David Desberg – </span><span class="illinois-orange-text">Undersecretary-General of Technology Operations</span><br>
+                    <span class="illinois-blue-text">Astha Saxena – </span><span class="illinois-orange-text">Deputy Undersecretary-General of Technology Operations</span><br>
+                    <span class="illinois-blue-text">Pavel Tamas – </span><span class="illinois-orange-text">Undersecretary-General of Simulations</span><br>
+                    <span class="illinois-blue-text">Muhammad Yousuf – </span><span class="illinois-orange-text">President of the General Assembly</span><br>
+                    <span class="illinois-blue-text">Mark Brisske – </span><span class="illinois-orange-text">Undersecretary-General of Rules and Procedures</span><br>
+                    <span class="illinois-blue-text">Manisha Venkat – </span><span class="illinois-orange-text">Undersecretary-General of Publications and Design</span><br>
+                    <span class="illinois-blue-text">Peggy Kern – </span><span class="illinois-orange-text">Undersecretary-General of Merchandising and Public Relations</span><br>
                 </p>
             </div>
         </div>
