408d9197a0e138268aec3722e9ff2d689a6f1ef6
David Desberg <david@daviddesberg.com>
2016-09-19 03:15:17 -0500

    merp

--- public/contact.php
+++ public/contact.php
@@ -13,9 +13,32 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Contact</h1>
-                <p>
-                    We will provide contact information shortly.
-                </p>
+                <h5>Send any inquiries below, and we'll get back to you as soon as possible.</h5>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="input-name" class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="input-name" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="input-email" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" name="email" id="input-email" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="input-message" class="col-sm-2 control-label">Message</label>
+                        <div class="col-sm-10">
+                            <textarea placeholder="Message" class="form-control" id="input-message" name="message"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-primary">Send</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
