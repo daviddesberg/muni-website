6dc9eddb9758aec71c236ac2808aaa2f5b2cc070
David Desberg <david@daviddesberg.com>
2016-10-02 22:56:28 -0500

    bios

--- public/secretariat.php
+++ public/secretariat.php
@@ -1,5 +1,13 @@
 <?php require_once('../incl/header.php'); ?>
 
+<style>
+    .secmember {
+        font-size: 22px;
+    }
+    .bio {
+
+    }
+</style>
 <header class="imageheader secheader">
     <div class="header-content">
         <div class="header-content-inner">
@@ -14,17 +22,50 @@
             <div class="col-lg-12">
                 <h1>Secretariat</h1>
                 <p>
-                    <span class="illinois-blue-text">Stuart Polen</span> – <span class="illinois-orange-text">Secretary-General</span></span><br>
-                    <span class="illinois-blue-text">Spencer Haydary – </span><span class="illinois-orange-text">Chief of Staff</span><br>
-                    <span class="illinois-blue-text">John Hall – </span><span class="illinois-orange-text">Undersecretary-General of Registration</span><br>
-                    <span class="illinois-blue-text">Danny Benson – </span><span class="illinois-orange-text">Undersecretary-General of Logistics and Operations</span><br>
-                    <span class="illinois-blue-text">David Desberg – </span><span class="illinois-orange-text">Undersecretary-General of Technology Operations</span><br>
-                    <span class="illinois-blue-text">Astha Saxena – </span><span class="illinois-orange-text">Deputy Undersecretary-General of Technology Operations</span><br>
-                    <span class="illinois-blue-text">Pavel Tamas – </span><span class="illinois-orange-text">Undersecretary-General of Simulations</span><br>
-                    <span class="illinois-blue-text">Muhammad Yousuf – </span><span class="illinois-orange-text">President of the General Assembly</span><br>
-                    <span class="illinois-blue-text">Mark Brisske – </span><span class="illinois-orange-text">Undersecretary-General of Rules and Procedures</span><br>
-                    <span class="illinois-blue-text">Manisha Venkat – </span><span class="illinois-orange-text">Undersecretary-General of Publications and Design</span><br>
-                    <span class="illinois-blue-text">Peggy Kern – </span><span class="illinois-orange-text">Undersecretary-General of Merchandising and Public Relations</span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Stuart Polen</span> – <span class="illinois-orange-text">Secretary-General</span></span></span><br>
+                    <span class="bio">
+                        I am a senior pursuing a double major in Operations Management and Marketing with a minor in International Development Economics. This will be my 6th and final year of Model United Nations and the 8th conference that I have staffed on the Midwestern Circuit. I have directed the Joint Crisis Committee for MUNI XIX and MUNI XX and I served as Chief of Staff for MUNI XXI. Model U.N. has been a large part of my life for a very long time and I am thrilled to be your Secretary General for MUNI XXII. When I’m not doing something Model U.N. related (which is rare), I like to play Settlers of Catan and watch B-movies like Rollertown.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Spencer Haydary – </span><span class="illinois-orange-text">Chief of Staff</span></span><br>
+                    <span class="bio">
+                        My name is Spencer Haydary, and I serve as the Chief of Staff for MUNI XXII. I’m originally from Rockford, IL and I am a Senior studying Psychology and Communication. One day, I hope to run a non-profit, and eventually to work for the United Nations, or serve in the Department of State. I am also involved in the Illinois Student Senate, where I serve as the Student Body Vice-President Internal. Outside of school, I am an Eagle Scout, and a Master in Tae Kwon Do.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">John Hall – </span><span class="illinois-orange-text">Undersecretary-General of Registration</span></span><br>
+                    <span class="bio">
+                        I am a sophomore majoring in Global Studies focusing in Governance, Conflict, and Resolution. In addition to my major, I am minoring in Russian, East European, and Eurasian Studies and pursuing certificates in Global Security and International Development. This is my second year of Model United Nations and second year being involved with MUNI. Last year, at MUNI XXI, I had a great time serving as crisis staff for the Irish Revolution committee. This year, I am excited to serve as the USG of Registration for MUNI XXII. Joining Model UN has easily been one of the best decisions of my college career and I plan on channeling my passion for it into ensuring that this year’s MUNI is the best one yet. Outside of living and breathing Model UN, I spend most of my time practicing guitar, working out, watching The Walking Dead and House of Cards, and crafting memes.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Danny Benson – </span><span class="illinois-orange-text">Undersecretary-General of Logistics and Operations</span></span><br>
+                    <span class="bio">
+I am a freshman here at Illinois, currently double majoring in Political Science and Communications.  Although this is my first year participating in MUNI, I have competed in Model United Nations for the past four years at my high school. I have staffed four committees during my career.  Model UN has been my favorite activity for the past five years of my life, and I am excited to facilitate a great experience for this year’s delegates. Outside of the political sphere, I enjoy playing intramural sports as well as beating my roommates at Super Smash Bros.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">David Desberg – </span><span class="illinois-orange-text">Undersecretary-General of Technology Operations</span></span><br>
+                    <span class="bio">
+I am a junior at the University of Illinois pursuing a degree in Computer Science. I have been involved with Model United Nations since my sophomore year of high school, and MUNI XXII marks the second time I have served on Secretariat. My goal is to ensure that every technology-related component of the conference &mdash; from the web site, to the registration process, to printing during the weekend of the conference &mdash; runs as smoothly as possible. Outside of Model UN, I enjoy playing basketball, listening to music, and crafting memes.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Astha Saxena – </span><span class="illinois-orange-text">Deputy Undersecretary-General of Technology Operations</span></span><br>
+                    <span class="bio">
+                        I am a senior majoring in Computer Science. I am an exchange student from University of York, UK and am here for 2 semesters. I have been part of Model United Nations at my School in Belfast, Northern Ireland. I enjoy Ice-skating and dancing, at my school I was a model in the our annual dance and modelling show. During my holidays I love to go Scuba Diving.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Pavel Tamas – </span><span class="illinois-orange-text">Undersecretary-General of Simulations</span></span><br>
+                    <span class="bio">
+I am a senior studying Finance and Accountancy with a minor in History. This is the 6th year of my my Model United Nations career and the 4th MUNI I have staffed. In the past, I have worked as a Simulations Director, Chair and Crisis Staffer, so running the department was my last stop on the simulations path. I am proud to help bring to life seven excellent committees this year and I know my staff are equally excited to as well. When not taking part in Model United Nations, I enjoy practicing karate, watching college football and reading The Economist.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Muhammad Yousuf – </span><span class="illinois-orange-text">President of the General Assembly</span></span><br>
+                    <span class="bio">
+                        I am a senior majoring in Philosophy with a double minor in Political Science and Gender and Women’s Studies. This is my eighth year in Model UN and my fourth year in Illinois Model UN, as well as my first year on secretariat. Additionally, I was the head of our competitive travel team my sophomore year. Model UN has been incredibly influential in my life, connecting me to some wonderful people and providing me with a skill set that I utilize in my academic and activist work. Outside of MUN, I’m a student activist and I love to cook and listen to death metal (sometimes at the same time!). I’m very excited to run the GAs this year, and I hope they prove to be the most challenging and exciting GAs at MUNI to date. I look forward to meeting many of you come March!
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Mark Brisske – </span><span class="illinois-orange-text">Undersecretary-General of Rules and Procedures</span></span><br>
+                    <span class="bio">
+                        I am a senior pursuing a double major in mathematics and economics with a minor in German. This will be my 6th and final year of Model United Nations and the 4th conference that I have had the pleasure of being a part of here at the University of Illinois. In my past, I have chaired two committees and I have led the chairs as USG of Parliamentary Procedure. I am thrilled to again this year be serving in this capacity. Please feel free to reach out to me via email with any questions, and I will try to to get back to you within a week or two.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Manisha Venkat – </span><span class="illinois-orange-text">Undersecretary-General of Publications and Design</span></span><br>
+                    <span class="bio">
+                        I am a junior in Advertising with a minor in French. This is my second year on the MUNI secretariat as the USG of Publications and Design. I am very excited to work with the International Press Delegation once again this year and I cannot wait to meet the team! I have been involved with Model UN since my freshmen year in high school and it has shaped my college career greatly. In my free time, I like to cook, eat and watch all the horror films on Netflix and Hulu.
+                    </span><br>
+                    <span class="secmember"><span class="illinois-blue-text">Peggy Kern – </span><span class="illinois-orange-text">Undersecretary-General of Merchandising and Public Relations</span></span><br>
+                    <span class="bio">
+                        I am a freshman pursuing an English major with a pre-law focus, (I one day hope to become a divorce lawyer). This will be my third year of doing Model United Nations, and first year at the collegiate level. I am very excited to serve as your USG of Merchandising this year! You can find me in the MUNI store at this year’s conference. In my free time I enjoy golfing, writing poetry, and watching Avatar: The Last Airbender.
+                    </span><br>
                 </p>
             </div>
         </div>
