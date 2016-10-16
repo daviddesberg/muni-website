3326bb729b265423f1469ef9661374a0b5a5ca0f
David Desberg <david@daviddesberg.com>
2016-10-16 16:24:43 -0500

    sponsorship

--- public/sponsorship.php
+++ public/sponsorship.php
@@ -14,7 +14,10 @@
             <div class="col-lg-12">
                 <h1>Sponsorship</h1>
                 <p>
-                    Sponsorship information coming soon.
+                    As a conference with appeal from high schools across the Midwest, we offer a great opportunity to promote your organization among hundreds of high school students and faculty advisors from across the region. We are committed to working with you to create a sponsorship package that best suits the needs of your organization. We encourage you to advertise through MUNI. Moreover, <strong>as a 501(c)(3) non-profit organization, contributions to Model United Nations at Illinois are tax-deductible.</strong>
+                </p>
+                <p>
+                    Please do not hesitate to contact <a href="mailto:merchandising@illinoismun.org">merchandising@illinoismun.org</a> with questions regarding sponsorship packages.
                 </p>
             </div>
         </div>
