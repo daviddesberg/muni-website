0e660b8c5c4e833ca2d12d5a4d046a8ee5057ab0
David Desberg <david@daviddesberg.com>
2016-10-16 16:27:32 -0500

    hotel

--- public/hotel.php
+++ public/hotel.php
@@ -14,7 +14,7 @@
             <div class="col-lg-12">
                 <h1>Hotel</h1>
                 <p>
-                    Hotel information will be available soon.
+                    We are proud to announce that MUNI XXII will be partnering with the Wyndham Garden in Urbana. The hotel is located 10 to 15 minutes away from the conference venues and transportation will be made available upon request. <strong>The Wyndham Garden is offering special rates for our conference attendees who will be staying at the hotel.</strong> To make arrangements, please call (217) 328-7900 and let the staff know that you are booking accommodations for MUNI.
                 </p>
             </div>
         </div>
