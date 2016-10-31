f305c2940daa36df4a5692417819cdf9b59abef5
David Desberg <david@daviddesberg.com>
2016-10-31 17:52:52 -0500

    letter

--- public/index.php
+++ public/index.php
@@ -21,6 +21,37 @@
                     <div class="pull-right" style="text-align:center; margin-left: 25px;">
                         <img style="height: 350px;" alt="Stuart J. Polen" src="assets/images/stuart.jpg"><br>
                     </div>
+
+                    <p>
+                    Prospective Advisors, Delegates, and Sponsors,
+                    </p>
+                    <p>
+                    It is my honor to welcome you to the twenty-second session of Model United Nations at the University of Illinois (MUNI)! The conference will be held between March 10-12, 2017.
+                    </p>
+
+                    <p>
+                        I would like to reflect upon how MUNI’s vision and mission have changed over the course of its history. When this conference began twenty-two years ago, our initial mission was to show delegates an idealized version of the world in which the United Nations can solve even the most complex of issues. We have highlighted the importance of diplomacy and consensus building while prioritizing delegate education. However, the world is a fundamentally different place than it was back during MUNI I. Nowadays, Model United Nations is no longer just about showing the world as it ought to be. It is the duty of those who run Model United Nations conferences to bridge the gap between the idealized vision of the world and how the world truly operates. I truly believe that MUNI XXII has become a vehicle for delegates to explore how to solve current problems while not getting bogged down in how the world ought to be. Given the importance of political realism at all levels of diplomacy and foreign affairs, <strong>the chief goal of MUNI XXII is to create a well-rounded citizen by challenging delegates to solve problems in realistic political environments.</strong>
+                    </p>
+                    <p>
+                        In the spirit of this mission, MUNI XXII will do something that has never been done before on the high school circuit. This year, MUNI will take the dynamic of the collegiate Model United Nations circuit and translate it for the high school audience. On the collegiate circuit, political realism is the forefront of every debate, which adds some interesting aspects to discourse. By emulating this style, MUNI will be an experience that no delegate will ever forget. We will be featuring some specialized committees and crisis committees, including a historical Russian Duma and the Illinois General Assembly. However, we will also present various U.N bodies that high school students know and enjoy, including DISEC, the WHO, the UNHRC, and others. While we will have some nontraditional committees this year, we will maintain the personal touch that delegates and advisors enjoy about MUNI. Ultimately, the diversity of our committees and subject matters will provide something for delegates of every persuasion. Whether your delegates enjoy GA committees, Crisis Committees, or something in between, MUNI provides something for everyone.
+                    </p>
+                    <p>
+                        On behalf of the Secretariat and the rest of our staff, I would like to thank you for considering MUNI XXII to your schedule and we hope to see you in March! If you have any questions regarding registration, please do not hesitate to contact us at <script>document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'&'+'#'+'1'+'0'+'9'+';'+'a'+'i'+'l'+'&'+'#'+'1'+'1'+'6'+';'+'o'+
+                                '&'+'#'+'5'+'8'+';'+'%'+'&'+'#'+'5'+'5'+';'+'2'+'e'+'%'+'&'+'#'+'5'+'4'+';'+'7'+'&'+'#'+'1'+'0'+'5'+
+                                ';'+'s'+'t'+'r'+'&'+'#'+'9'+'7'+';'+'&'+'#'+'1'+'1'+'6'+';'+'%'+'6'+'9'+'o'+'%'+'6'+'&'+'#'+'6'+'9'+
+                                ';'+'%'+'4'+'0'+'i'+'l'+'l'+'%'+'6'+'9'+'&'+'#'+'1'+'1'+'0'+';'+'o'+'i'+'s'+'m'+'&'+'#'+'1'+'1'+'7'+
+                                ';'+'&'+'#'+'3'+'7'+';'+'&'+'#'+'5'+'4'+';'+'E'+'%'+'2'+'&'+'#'+'6'+'9'+';'+'o'+'r'+'g'+"'"+'>'+'&'+
+                                '#'+'1'+'1'+'4'+';'+'e'+'g'+'i'+'s'+'t'+'&'+'#'+'1'+'1'+'4'+';'+'a'+'t'+'i'+'o'+'n'+'&'+'#'+'6'+'4'+
+                                ';'+'i'+'l'+'l'+'i'+'n'+'o'+'i'+'s'+'&'+'#'+'1'+'0'+'9'+';'+'u'+'n'+'&'+'#'+'4'+'6'+';'+'o'+'&'+'#'+
+                                '1'+'1'+'4'+';'+'g'+'<'+'/'+'a'+'>');</script><noscript>[Turn on JavaScript to see the email address]</noscript>.
+                    </p>
+                    <p>
+                    Sincerely,<br>
+                    Stuart J. Polen<br>
+                    Secretary General MUNI XXII
+                    </p>
+
+                    <?php /*
                     <p>
                     Prospective Advisors and Delegates,</p>
 
@@ -34,10 +65,8 @@
                     <p style="text-indent: 30px;">
                         On behalf of the Secretariat, I hope you consider adding MUNI XXII to your schedule. We take great pride in our conference and we hope to see you in March! If you have any questions about registering for MUNI XXII, please do not hesitate to contact our Undersecretary-General of Registration at registration@illinoismun.org.
                     </p>
+                    */ ?>
 
-                    Sincerely,<br>
-                    Stuart J. Polen<br>
-                    Secretary General MUNI XXII
                 </div>
             </div>
         </div>
