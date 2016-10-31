ac83df914e9e6af886ad19a98fd3803da1fe0220
David Desberg <david@daviddesberg.com>
2016-10-31 10:25:37 -0500

    bork

--- public/crisis.php
+++ public/crisis.php
@@ -12,18 +12,24 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12" style="text-align: left;">
-                <p>
-                <ul style="list-style-type: none; font-size: 36px;" class="headerfont">
-                    <li><a href="crisispage.php?c=0">Kingdom of Heaven: The Court of Baldwin IV of Jerusalem</a></li>
-                    <li><a href="crisispage.php?c=1">The Next Great City: The FIFA Executive Board Committee</a></li>
-                    <li><a href="crisispage.php?c=2">Destiny Awaits: Iraqi Government (JCC)</a></li>
-                    <li><a href="crisispage.php?c=3">Responsibility to Protect: Coalition Forces (JCC)</a></li>
-                    <li><a href="crisispage.php?c=4">Independence: Nehru&#39;s Cabinet of 1947</a></li>
-                    <li><a href="crisispage.php?c=5">Mars Colony, 2037: Red Alert on the Red Planet</a></li>
-                    <li><a href="crisispage.php?c=6">Island of Mystery: The Roanoke Colony</a></li>
-                    <li><a href="crisispage.php?c=7">UNSC 2017: The World at Crossroads</a></li>
-                </ul>
-                </p>
+                <table class="table" style="font-size: 36px; width: 100%;">
+                    <tr>
+                        <td><a href="crisispage.php?c=0">Kingdom of Heaven: The Court of Baldwin IV of Jerusalem</a></td>
+                        <td><a href="crisispage.php?c=1">The Next Great City: The FIFA Executive Board Committee</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="crisispage.php?c=2">Destiny Awaits: Iraqi Government (JCC)</a></td>
+                        <td><a href="crisispage.php?c=3">Responsibility to Protect: Coalition Forces (JCC)</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="crisispage.php?c=4">Independence: Nehru&#39;s Cabinet of 1947</a></td>
+                        <td><a><em>Ad Hoc</em> Committee of the Secretary-General</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="crisispage.php?c=6">Island of Mystery: The Roanoke Colony</a></td>
+                        <td><a href="crisispage.php?c=7">UNSC 2017: The World at Crossroads</a></td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
