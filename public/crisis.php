23f19767c9c32688dcc571d7c3f9741fabfbce82
David Desberg <david@daviddesberg.com>
2016-10-31 10:20:41 -0500

    fix thing

--- public/crisis.php
+++ public/crisis.php
@@ -16,7 +16,7 @@
                 <ul style="list-style-type: none; font-size: 36px;" class="headerfont">
                     <li><a href="crisispage.php?c=0">Kingdom of Heaven: The Court of Baldwin IV of Jerusalem</a></li>
                     <li><a href="crisispage.php?c=1">The Next Great City: The FIFA Executive Board Committee</a></li>
-                    <li><a href="crisispage.php?c=2">Destiny Awaits: Iraqi Government (JCC</a></li>
+                    <li><a href="crisispage.php?c=2">Destiny Awaits: Iraqi Government (JCC)</a></li>
                     <li><a href="crisispage.php?c=3">Responsibility to Protect: Coalition Forces (JCC)</a></li>
                     <li><a href="crisispage.php?c=4">Independence: Nehru&#39;s Cabinet of 1947</a></li>
                     <li><a href="crisispage.php?c=5">Mars Colony, 2037: Red Alert on the Red Planet</a></li>
