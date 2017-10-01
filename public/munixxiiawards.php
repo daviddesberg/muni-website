a362021824f7c1135270d918f0a0ca985925f33a
David Desberg <david@daviddesberg.com>
2017-10-01 16:57:37 -0500

    ope

---
+++ public/munixxiiawards.php
@@ -0,0 +1,147 @@
+<?php require_once('../incl/header.php'); ?>
+
+<header class="imageheader hotelheader">
+    <div class="header-content">
+        <div class="header-content-inner">
+            <h1 id="main-heading"><span>MUNI XXII Awards</span></h1>
+        </div>
+    </div>
+</header>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Award Winners</h1>
+                <h2 class="illinois-orange-text">Delegation Award Winners</h2>
+                <div style="font-size: 18px;">
+                Best Large Delegation: Lyons Township High School<br>
+                Outstanding Large Delegation: Adlai E. Stevenson High School<br>
+                Best Small Delegation: Edgewood High School<br>
+                Outstanding Small Delegation: Antioch Community High School
+                </div>
+                <h2 class="illinois-blue-text">Committee Award Winners</h2>
+		        <h3>ECOSOC</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Andorra (Lyons Township)</li>
+                    <li>Outstanding Delegate: Iraq (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Russian Federation (Lyons Township)</li>
+                    <li>Honorable Mention: Portugal (Pewaukee)</li>
+                    <li>Verbal Commendation: Venezuela (Bolivarian Republic of) (Downers Grove North)</li>
+		        </ul>
+		        <h3>UNHRC</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Russian Federation (Lyons Township)</li>
+                    <li>Outstanding Delegate: Venezuela (Bolivarian Republic of) (Downers Grove North)</li>
+                    <li>Honorable Mention: France (St. Ignatius)</li>
+                    <li>Honorable Mention: Georgia (Edgewood)</li>
+                    <li>Verbal Commendation: Panama (St. Ignatius)</li>
+		        </ul>
+		        <h3>WHO</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: France (St. Ignatius)</li>
+                    <li>Outstanding Delegate: Mexico (Antioch)</li>
+                    <li>Honorable Mention: United States of America (William Fremd)</li>
+                    <li>Honorable Mention: Viet Nam (St. Ignatius)</li>
+                    <li>Verbal Commendation: United Kingdom (White Station)</li>
+		        </ul>
+		        <h3>DISEC</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: United States of America (William Fremd)</li>
+                    <li>Outstanding Delegate: China (People's Republic of) (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Russian Federation (Lyons Township)</li>
+                    <li>Honorable Mention: Nigeria (St. Ignatius)</li>
+                    <li>Verbal Commendation: Czech Republic (Willowbrook)</li>
+		        </ul>
+		        <h3>ECOFIN</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: France (St. Ignatius)</li>
+                    <li>Outstanding Delegate: United States of America (William Fremd)</li>
+                    <li>Honorable Mention: Russian Federation (Lyons Township)</li>
+                    <li>Honorable Mention: United Kingdom (White Station)</li>
+                    <li>Verbal Commendation: China (People's Republic of) (Adlai E. Stevenson)</li>
+		        </ul>
+		        <h3>Illinois General Assembly</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Rep. Jeanne Ives (R-42) (White Station)</li>
+                    <li>Outstanding Delegate: Rep. Sara Wojcicki Jimenez (R-99) (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Rep. Thomas Morrison (R-54) (St. Ignatius)</li>
+                    <li>Honorable Mention: Rep. Barbara Flynn Currie (D-25) (Lyons Township)</li>
+                    <li>Verbal Commendation: Rep. Kathleen Willis (D-77) (Lyons Township)</li>
+		        </ul>
+		        <h3>1917 Russian Duma</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Roma Franzkiewicz (Lyons Township)</li>
+                    <li>Outstanding Delegate: Alexander Kerensky (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Alex Bukoviki (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Sergey Kadyrov (Lyons Township)</li>
+                    <li>Verbal Commendation: Ivan Goremykin (St. Ignatius)</li>
+		        </ul>
+		        <h3>International Press Delegation</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Kavitha Babu: Washington Post (Pewaukee)</li>
+                    <li>Verbal Commendation: Alexandra Chaidez: The Moscow Times (St. Ignatius)</li>
+		        </ul>
+		        <h3>Ad-Hoc: Colonization of Mars</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Albina Belyakova: Director of Health (Lyons Township)</li>
+                    <li>Outstanding Delegate: Barbara Koenig: Director of Agriculture (Neuqua Valley)</li>
+                    <li>Honorable Mention: Jeannine Sauvé: Director of Interplanetary Communications (St. Ignatius)</li>
+                    <li>Verbal Commendation: Ying Lai: CNSA Representative (Adlai E. Stevenson)</li>
+		        </ul>
+		        <h3>JCC: Gulf War</h3>
+                <h4>Coalition Forces</h4>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Mustafa Tlass: Minister of Defense, Syria (Lyons Township)</li>
+                    <li>Outstanding Delegate: Safa Giray: Minister of National Defense, Turkey (Edgewood)</li>
+                    <li>Honorable Mention: Dick Cheney: Secretary of Defense, United States of America (William Fremd)</li>
+                    <li>Verbal Commendation: Youssef Sabri Abu Taleb: Minister of Defense, Egypt (Adlai E. Stevenson)</li>
+		        </ul>
+                <h4>Iraqi Cabinet</h4>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Sabawi Ibrahim al-Tikriti: Head of the Iraqi Intelligence Service (Adlai E. Stevenson)</li>
+                    <li>Outstanding Delegate: Mahmud Dhiyab: Minister of Agriculture (White Station)</li>
+                    <li>Honorable Mention: Tariq Aziz: Minister of Foreign Affairs (Lyons Township)</li>
+                    <li>Verbal Commendation: Issam al-Chalabi: Minister of Oil (Edgewood)</li>
+		        </ul>
+		        <h3>Roanoke Colony</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Oota dabun: Representative of the Croatoan Tribe (Lyons Township)</li>
+                    <li>Outstanding Delegate: Edward Powell: Capitan of the Red Lion (St. Ignatius)</li>
+                    <li>Honorable Mention: Eleanor Dare: Governors Daughter (Lyons Township)</li>
+                    <li>Verbal Commendation: Thomas Topan: Tobacco Farmer (White Station)</li>
+		        </ul>
+		        <h3>Kingdom of Jerusalem</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Heinrich von Bassenheim: Grand Master of the Knights Teutonic (Lyons Township)</li>
+                    <li>Outstanding Delegate: Raynald de Châtillon: Prince of Antioch (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Guy de Lusignan: Holy Land noble, rival of Montferrat (Lyons Township)</li>
+                    <li>Verbal Commendation: Odo de St. Amand: Grand Master of the Knight Templar (White Station)</li>
+		        </ul>
+		        <h3>Jawaharlal Nehru's Cabinet</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Sardar Vallabhai Patel: Deputy Prime Minister (White Station)</li>
+                    <li>Outstanding Delegate: R. K. Shanmukham Chetty: Minister of Finance (St. Ignatius)</li>
+                    <li>Honorable Mention: Maulana Abul Kalam Azad: Minister of Education (William Fremd)</li>
+                    <li>Verbal Commendation: C. H. Bhabha: Minister of Commerce (Lyons Township)</li>
+		        </ul>
+		        <h3>United Nations Security Council</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Russian Federation (Lyons Township)</li>
+                    <li>Outstanding Delegate: United Kingdom (White Station)</li>
+                    <li>Honorable Mention: France (St. Ignatius)</li>
+                    <li>Verbal Commendation: United States (William Fremd)</li>
+		        </ul>
+		        <h3>FIFA Executive Committee</h3>
+		        <ul style="list-style-type: none;">
+                    <li>Best Delegate: Sepp Blatter: President (Lyons Township)</li>
+                    <li>Outstanding Delegate: Julio Grondona: Senior Vice President, Finance Committee Chairman (Adlai E. Stevenson)</li>
+                    <li>Honorable Mention: Angel Maria Villar Llona (White Station)</li>
+                    <li>Verbal Commendation: Ricardo Terra Teixeira: President of the CBF, FIFA Executive Committee Member (Columbia Independent)</li>
+		        </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php require_once('../incl/footer.php'); ?>
