beaf9c3db54824f40b5043f7335995ac9f174036
Archit Gupta <architgupta@wirelessprv-10-194-74-115.near.illinois.edu>
2017-10-01 15:55:53 -0500

    ga letter update

--- public/ga.php
+++ public/ga.php
@@ -18,20 +18,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12" style="text-align: left;">
-                <table id="crisislinks" class="table" style="font-size: 36px; width: 100%;">
-                    <tr>
-                        <td><a class="comlink" href="gapage.php?c=disec">Disarmament and International Security Committee (DISEC)</a></td>
-                        <td><a class="comlink" href="gapage.php?c=ecosoc">Economic and Social Council (ECOSOC)</a></td>
-                    </tr>
-                    <tr>
-                        <td><a class="comlink" href="gapage.php?c=ecofin">Economic and Financial Affairs Council (ECOFIN)</a></td>
-                        <td><a class="comlink" href="gapage.php?c=who">World Health Organization (WHO)</a></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" style="text-align: center;"><a class="comlink" href="gapage.php?c=unhrc">United Nations Human Rights Council (UNHRC)</a></td>
-
-                    </tr>
-                </table>
+                <p>
+                Hi everyone, my name is Gordon Young and I am thrilled to be serving as the President of the General Assembly this year at MUNI. I am currently a Sophomore studying Political Science at Illinois and have participated in Model UN since my Freshman year of high school. Outside of school, I help to run the CIMUN conference in Chicago and serve as the Staff Development Program director at a summer camp in Wisconsin. This year at MUNI, our vision for the General Assembly committees is to have them cover topics that are relevant to our world today and can give the delegates insights into the issues in a way that no other experience can. Since my Freshman year of high school, I have participated in Model UN and it has been an experience unlike any other. From representing a broad range of nations to covering a unique spread of topics at conferences around the world, I was able to learn things that were beyond the reach of the average high school student. My own Model UN experience has inspired me to transform the committees at MUNI this year such that every delegate walks away from committee with the same sense of knowledge gained.
+                </p>
+                <p>
+                Our other goal this year for our committees in general is to help all the delegates develop skills, that not only will help them in future conferences, but will help them succeed at other things in life. We ensure this by only choosing our best and brightest members to help design and lead the committees and help the delegates along the way such that every delegate, regardless of them winning an award or not, walks away from the conference on Sunday having left their comfort zone and put a new set of skills to the test.
+                </p>
+                <p>
+                For anyone who is interested in more information on the General Assembly this year or Model UN at Illinois in general, feel free to email me at <a href="mailto:pga@illinoismun.org">pga@illinoismun.org</a>.
+                </p>
+                <p>
+                Gordon Young
+                </p>
             </div>
         </div>
     </div>
