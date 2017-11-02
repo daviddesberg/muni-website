7dbc3a3e8578ac9568ccceb4fc159cbebd41acc6
Archit Gupta <archit.941@gmail.com>
2017-11-01 21:15:24 -0500

    aaaa

--- public/hotel.php
+++ public/hotel.php
@@ -14,14 +14,27 @@
             <div class="col-lg-6">
                 <h1>Hotel</h1>
                 <p>
-                We are pleased to announce that we have partnered with Hilton for our official hotels this year! This year, we have set our sights on assuring our conference not only improves in educational and skill development value, but in overall comfort and experience as well. In partnering with Hilton, we believe we have achieved this goal. There are three hotels that we have partnered with. They are: the Hilton Garden Inn, the Homewood Suites by Hilton, and the Home2 Suites by Hilton all located in Champaign.
-                The primary reason we chose to partner with multiple hotels is to provide each delegation with the opportunity to select what works best for them. Each hotel is unique in what they offer from room types to overall atmosphere but they all have a common denominator in comfort and quality customer service.
+                We are pleased to announce that we have partnered with Hilton for our official hotels this year! This year, we have set our sights on assuring our conference not only improves in educational and skill development value, but in overall comfort and experience as well. In partnering with Hilton, we believe we have achieved this goal. There are three hotels that we have partnered with. They are: the Hilton Garden Inn, the Homewood Suites by Hilton, and the Home2 Suites by Hilton all located in Champaign. The primary reason we chose to partner with multiple hotels is to provide each delegation with the opportunity to select what works best for them. Each hotel is unique in what they offer from room types to overall atmosphere but they all have a common denominator in comfort and quality customer service.
+                </p>
+                <p>
                 In meeting with the Hilton group, we have secured a special rate for MUNI delegations of $140 per room, per night across all three hotels. Furthermore, upscale continental breakfast options are included at both the Homewood and Home2. At the Hilton Garden Inn, a full hot breakfast is available with made to order options. Across all three hotels, Wi-Fi is included in your rates along with the option to use the lobby of the hotels as a common area to do work or even throw a pizza party with your delegation, free of charge. 
+                </p>
+                <p>
                 As far as logistics and check in procedures are concerned, please view our Registration Guidebook on the Registration page of the website for details. 
+                </p>
+                <p>
                 More extensive information on dining options at and around the hotels, services offered, pictures, and more will be posted soon! For now, please view the websites for each hotel at the links below for more information.
+                </p>
+                <p>
                 Once you register for the conference and pay your school fee, you will then be sent the special code/link to book rooms at the hotel of your choosing with at the rate of $140. This means you should register soon and pay your school fee early to assure you secure rooms at the hotel of your choice! For delegations that have their school fee waived, this does not apply to you.
+                </p>
+                <p>
                 <a href  = "http://hiltongardeninn3.hilton.com/en/hotels/illinois/hilton-garden-inn-champaign-urbana-CMICHGI/index.html"> Hilton Garden Inn
+                </p>
+                <p>
                 <a href  = "http://homewoodsuites3.hilton.com/en/hotels/illinois/homewood-suites-by-hilton-champaign-urbana-CMIHWHW/index.html"> Homewood Suites
+                </p>
+                <p>
                 <a href  = "http://home2suites3.hilton.com/en/hotels/illinois/home2-suites-by-hilton-champaign-urbana-CMIURHT/index.html"> Home2 Suites
                 </p>
             </div>
